f08b9465525bb3f71c3f9249cbfdc7a4494f6959
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-10-05 19:05:06 +0200

    updated according check

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -59,21 +59,23 @@ router.delete('/:contactId', async (req, res, next) => {
 })
 
 
-router.put('/:contactId', async (req, res, next) => {
-	const { error } = contactSchema.validate(req.body)
-	if (error) return res.status(400).json({ message: error.details[0].message })
-	const { name, email, phone } = req.body
-	const { contactId } = req.params
-	if (!name && !email && !phone) {
-		res.status(400).json({ message: 'missing fields' })
-	}
-	const contact = await updateContact(contactId, req.body)
-	if (contact) {
-		res.status(200).json(contact)
-	} else {
-		res.status(404).json({ message: 'Not found' })
-	}
-})
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "missing fields" });
+    }
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+    if (!result) {
+      res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ result });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 
 module.exports = router
