ef89d5f0b893f9cfc62a116326448ae2d6cc20bc
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-10-13 00:34:27 +0200

    added HW03

--- models/contacts.js
+++ models/contacts.js
@@ -65,6 +65,28 @@ const removeContact = async (contactId) => {
     }
 }
 
+const updateStatusContact = async (req, res, next) => {
+	try {
+		const { error } = contactValidator(req.body)
+		if (error) return res.status(400).json({ message: error.details[0].message })
+		const { favorite } = req.body
+		const { contactId } = req.params
+		if (!favorite && favorite !== false) {
+			res.status(400).json({ message: 'missing field favorite' })
+		}
+		const contact = await service.updateStatusContact(contactId, favorite)
+
+		if (contact) {
+			res.status(200).json(contact)
+		} else {
+			res.status(404).json({ message: 'Not found' })
+		}
+	} catch (error) {
+		console.error(error.message)
+		next(error)
+	}
+}
+
 const updateContact = async (contactId, body) => {
 	const contacts = await listContacts()
 	const contactToUpdate = contacts.find(contact => contact.id === contactId)
@@ -91,4 +113,5 @@ module.exports = {
 	removeContact,
 	addContact,
 	updateContact,
+	updateStatusContact,
 }
