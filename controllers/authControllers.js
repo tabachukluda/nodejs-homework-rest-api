44aba670975e963971ce1431aa6b939c04297fc6
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-10-22 00:09:51 +0200

    added first part HW

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,7 +6,14 @@ const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 
+const fs = require('fs/promises');
+const path = require("path");
+const gravatar = require("gravatar")
+const Jimp = require("jimp");
+
 const { JWT_SECRET } = process.env
+
+const avatarsPath = path.resolve("public", "avatars");
 
 const { userSignupSchema, userSigninSchema } = require('../schemas/validator.js');
 
@@ -16,11 +23,9 @@ const register = async (req, res) => {
     if (!req.body || Object.keys(req.body).length === 0) {
         return res.status(400).json({ message: 'missing field' });
     }
-
     const { error } = userSignupSchema.validate(req.body);
-
     if (error) return res.status(400).json({ message: error.details[0].message });
-
+    try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
@@ -29,9 +34,16 @@ const register = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const avatarURL = gravatar.url(email, { s: "250", d: "retro" });
 
-    res.status(201).json({ user: { email: newUser.email, subscription: newUser.subscription } });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+
+    res.status(201).json({
+        user: { email: newUser.email, subscription: newUser.subscription }
+    });
+    } catch (error) {
+    res.status(500).json({ message: error.message });
+    }
 }
 
 
@@ -87,11 +99,38 @@ const logout = async(req, res)=> {
     await User.findByIdAndUpdate(_id, {token: ""});
 
     return res.status(204).json({});
+}
+    
+const updateAvatar = async (req, res) => {
+    if (!req.user) {
+        return res.status(401).json({ message: "Not authorized" });
     }
+
+    try {
+        const { _id } = req.user;
+        const { path: tempUpload, originalname } = req.file;
+        const filename = `${_id}_${originalname}`;
+        const resultUpload = path.resolve(avatarsPath, filename); 
+        const avatarURL = path.join("avatars", filename); 
+
+        
+        await fs.rename(tempUpload, resultUpload);
+
+        await User.findByIdAndUpdate(_id, { avatarURL });
+
+        res.status(200).json({ avatarURL });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
 
 module.exports = {
     register: ctrlWrapper(register),  
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
